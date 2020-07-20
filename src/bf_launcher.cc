6ed5e8512f203877f4419770c75b8baad81cb101
Casey Doran <caseyd.doran@gmail.com>
2020-07-19 21:56:14 -0700

    make a simple grpc server

--- src/bf_launcher.cc
+++ src/bf_launcher.cc
@@ -4,102 +4,56 @@
 
 #include "steam.grpc.pb.h"
 #include <grpcpp/grpcpp.h>
-#include <grpc/support/log.h>
 
-/*
-using grpc::Channel;
-using grpc::ClientAsyncResponseReader;
-using grpc::ClientContext;
-using grpc::CompletionQueue;
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
 using grpc::Status;
-using steam::StateUpdate;
-using steam::DetailsUpdate;
+using steam::Steam;
+using steam::SteamworksInitRequest;
+using steam::SteamworksInitResult;
+using steam::StateUpdateMessage;
+using steam::StateUpdateAck;
+using steam::DetailsUpdateMessage;
+using steam::DetailsUpdateAck;
 
+class SteamServiceImpl final : public Steam::Service {
+    Status InitSteamworks(ServerContext* context, const SteamworksInitRequest* request, SteamworksInitResult* result){
+        std::cout << "Received Initialization request." << std::endl;
+        result;
+        return Status::OK;
 
-class GreeterClient {
- public:
-  explicit GreeterClient(std::shared_ptr<Channel> channel)
-      : stub_(Greeter::NewStub(channel)) {}
-
-  // Assembles the client's payload, sends it and presents the response back
-  // from the server.
-  std::string SayHello(const std::string& user) {
-    // Data we are sending to the server.
-    HelloRequest request;
-    request.set_name(user);
-
-    // Container for the data we expect from the server.
-    HelloReply reply;
-
-    // Context for the client. It could be used to convey extra information to
-    // the server and/or tweak certain RPC behaviors.
-    ClientContext context;
-
-    // The producer-consumer queue we use to communicate asynchronously with the
-    // gRPC runtime.
-    CompletionQueue cq;
-
-    // Storage for the status of the RPC upon completion.
-    Status status;
-
-    // stub_->PrepareAsyncSayHello() creates an RPC object, returning
-    // an instance to store in "call" but does not actually start the RPC
-    // Because we are using the asynchronous API, we need to hold on to
-    // the "call" instance in order to get updates on the ongoing RPC.
-    std::unique_ptr<ClientAsyncResponseReader<HelloReply> > rpc(
-        stub_->PrepareAsyncSayHello(&context, request, &cq));
-
-    // StartCall initiates the RPC call
-    rpc->StartCall();
-
-    // Request that, upon completion of the RPC, "reply" be updated with the
-    // server's response; "status" with the indication of whether the operation
-    // was successful. Tag the request with the integer 1.
-    rpc->Finish(&reply, &status, (void*)1);
-    void* got_tag;
-    bool ok = false;
-    // Block until the next result is available in the completion queue "cq".
-    // The return value of Next should always be checked. This return value
-    // tells us whether there is any kind of event or the cq_ is shutting down.
-    GPR_ASSERT(cq.Next(&got_tag, &ok));
-
-    // Verify that the result from "cq" corresponds, by its tag, our previous
-    // request.
-    GPR_ASSERT(got_tag == (void*)1);
-    // ... and that the request was completed successfully. Note that "ok"
-    // corresponds solely to the request for updates introduced by Finish().
-    GPR_ASSERT(ok);
-
-    // Act upon the status of the actual RPC.
-    if (status.ok()) {
-      return reply.message();
-    } else {
-      return "RPC failed";
     }
-  }
-
- private:
-  // Out of the passed in Channel comes the stub, stored here, our view of the
-  // server's exposed services.
-  std::unique_ptr<Greeter::Stub> stub_;
+    Status UpdateGameState(ServerContext* context, const StateUpdateMessage* message, StateUpdateAck* result){
+        std::cout << "Got a state update message: " << message->state() << std::endl;
+        return Status::OK;
+    }
+    Status UpdateGameDetails(ServerContext* context, const DetailsUpdateMessage* message, DetailsUpdateAck* result){
+        std::cout << "Got a details update message: " << message->details() << std::endl;
+        return Status::OK;
+    }
 };
 
-int main(int argc, char** argv) {
-  // Instantiate the client. It requires a channel, out of which the actual RPCs
-  // are created. This channel models a connection to an endpoint (in this case,
-  // localhost at port 50051). We indicate that the channel isn't authenticated
-  // (use of InsecureChannelCredentials()).
-  GreeterClient greeter(grpc::CreateChannel(
-      "localhost:50051", grpc::InsecureChannelCredentials()));
-  std::string user("world");
-  std::string reply = greeter.SayHello(user);  // The actual RPC call!
-  std::cout << "Greeter received: " << reply << std::endl;
+void runServer(){
+    std::string server_address("0.0.0.0:50051");
+    SteamServiceImpl service;
+    ServerBuilder builder;
+    
+    // Listen on the given address without any authentication mechanism.
+    builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+    // Register "service" as the instance through which we'll communicate with
+    // clients. In this case it corresponds to an *synchronous* service.
+    builder.RegisterService(&service);
+    // Finally assemble the server.
+    std::unique_ptr<Server> server(builder.BuildAndStart());
+    std::cout << "Server listening on " << server_address << std::endl;
 
-  return 0;
+    // Wait for the server to shutdown. Note that some other thread must be
+    // responsible for shutting down the server for this call to ever return.
+    server->Wait();
 }
-*/
 
 int main(int argc, char** argv){
-    std::cout << "Launcher initialized." << std::endl;
+    runServer();
     return 0;
 }
