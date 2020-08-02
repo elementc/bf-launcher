ef7b46e171118f80f6fe40bf7c18973c0f2549b0
Casey Doran <caseyd.doran@gmail.com>
2020-08-02 16:24:45 -0700

    Gate gRPC initialization for the bitfighter launcher.

--- src/bf_launcher.cc
+++ src/bf_launcher.cc
@@ -21,6 +21,10 @@ using steam::StateUpdateAck;
 using steam::DetailsUpdateMessage;
 using steam::DetailsUpdateAck;
 
+// Blocks launching bitfighter until the grpc server is finished starting up.
+std::mutex grpc_init_gate;
+
+// Implementation of the Steamworks protocol.
 class SteamServiceImpl final : public Steam::Service {
     public: 
         Status InitSteamworks(ServerContext* context, const SteamworksInitRequest* request, SteamworksInitResult* result){
@@ -49,41 +53,67 @@ class SteamServiceImpl final : public Steam::Service {
         bool is_init = false;
     };
 
+// Thread entry point for the gRPC server.
 void runServer(){
+    
     std::string server_address("localhost:50051");
     SteamServiceImpl service;
+
     ServerBuilder builder;
     
     // Listen on the given address without any authentication mechanism.
     builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+
     // Register "service" as the instance through which we'll communicate with
     // clients. In this case it corresponds to an *synchronous* service.
     builder.RegisterService(&service);
-    // Finally assemble the server.
+
+    // Finally, assemble the server.
     std::unique_ptr<Server> server(builder.BuildAndStart());
     std::cout << "Server listening on " << server_address << std::endl;
+
+    grpc_init_gate.unlock();
 
     // Wait for the server to shutdown. Note that some other thread must be
     // responsible for shutting down the server for this call to ever return.
     server->Wait();
 }
 
+// Thread entry point for the Bitfighter process.
 void start_proc(const std::string proc){
-            TinyProcessLib::Process bf_process(proc, "", [](const char* bytes, size_t n) {
-            std::cout << "Output from stdout: " << std::string(bytes, n) << std::endl;
-            });
-            auto exit_status = bf_process.get_exit_status();
+
+    // Block until gRPC is finished starting up.
+    grpc_init_gate.lock();
+
+    // Launch bitfighter.
+    TinyProcessLib::Process bf_process(proc, "", [](const char* bytes, size_t n) {
+    std::cout << "Output from stdout: " << std::string(bytes, n) << std::endl;
+    });
+
+    // Block until bitfighter terminates.
+    auto exit_status = bf_process.get_exit_status();
 }
 
+// Application entry point.
 int main(int argc, char** argv) {
+
+    // Make sure args are sane.
     if (argc != 2) {
         std::cerr << "Usage: " << argv[0] << " /path/to/a/bitfighter/executable" << std::endl;
         return -1;
     }
-    std::thread proc_runner(start_proc, std::string(argv[1]));
-    std::thread grpc_server(runServer);
 
+    // Start with the gRPC gate locked.
+    grpc_init_gate.lock();
+
+    // The two threads we manage.
+    std::thread grpc_server(runServer);
+    std::thread proc_runner(start_proc, std::string(argv[1]));
+
+    // Wait for Bitfighter to quit.
     proc_runner.join();
+
+    // Then let go of the gRPC server thread so we can shut down cleanly.
     grpc_server.detach();
 
     return 0;
