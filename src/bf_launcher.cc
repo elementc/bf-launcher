006676dce96c1157b4ffb8875069a7cc28711065
Casey Doran <caseyd.doran@gmail.com>
2020-08-02 21:30:34 -0700

    use mutexes in a defined way

--- src/bf_launcher.cc
+++ src/bf_launcher.cc
@@ -8,6 +8,7 @@
 #include <process.hpp>
 #include <thread>
 #include <mutex>
+#include <chrono>
 
 using grpc::Server;
 using grpc::ServerBuilder;
@@ -20,9 +21,11 @@ using steam::StateUpdateMessage;
 using steam::StateUpdateAck;
 using steam::DetailsUpdateMessage;
 using steam::DetailsUpdateAck;
+using namespace std::chrono_literals;
 
 // Blocks launching bitfighter until the grpc server is finished starting up.
 std::mutex grpc_init_gate;
+bool grpc_is_started= false;
 
 // Implementation of the Steamworks protocol.
 class SteamServiceImpl final : public Steam::Service {
@@ -55,6 +58,8 @@ class SteamServiceImpl final : public Steam::Service {
 
 // Thread entry point for the gRPC server.
 void runServer(){
+
+    grpc_init_gate.lock();
     
     std::string server_address("localhost:50051");
     SteamServiceImpl service;
@@ -72,6 +77,7 @@ void runServer(){
     std::unique_ptr<Server> server(builder.BuildAndStart());
     std::cout << "Server listening on " << server_address << std::endl;
 
+    grpc_is_started = true;
     grpc_init_gate.unlock();
 
     // Wait for the server to shutdown. Note that some other thread must be
@@ -83,7 +89,14 @@ void runServer(){
 void start_proc(const std::string proc){
 
     // Block until gRPC is finished starting up.
-    grpc_init_gate.lock();
+    while(true){
+        grpc_init_gate.lock();
+        if (grpc_is_started){
+            break;
+        }
+        grpc_init_gate.unlock();
+        std::this_thread::sleep_for(1ms);
+    }
 
     // Launch bitfighter.
     TinyProcessLib::Process bf_process(proc, "", [](const char* bytes, size_t n) {
@@ -103,8 +116,6 @@ int main(int argc, char** argv) {
         return -1;
     }
 
-    // Start with the gRPC gate locked.
-    grpc_init_gate.lock();
 
     // The two threads we manage.
     std::thread grpc_server(runServer);
